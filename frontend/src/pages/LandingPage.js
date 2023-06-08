231031e006e6c4d8709c03507e8448718082096b
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2023-06-08 06:19:58 -0400

    more UI effects with state trigger, correct answer and wrong feedback, score and high score set with cookie

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,30 +1,39 @@
 import { React, useEffect, useState } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
+import { useCookies } from "react-cookie";
+import Logo from "../static/logo.png";
 
 const LandingPage = () => {
-  /* eslint-disable no-unused-vars */
+  /* eslint-disable no-unused-vars,react/no-unknown-property */
   const [items, setItems] = useState();
   const [index, setIndex] = useState(0);
   const [answer, setAnswer] = useState("");
   const [score, setScore] = useState(0);
-  let form = document.getElementById('form');
-  let itemName = document.getElementById('item_name');
+  const [highScore, setHighScore] = useState(0);
+  const [cookies, setCookie] = useCookies(["high_score"]);
+  const [wobble, setWobble] = useState(0);
+  let form = document.getElementById("form");
+  let itemName = document.getElementById("item_name");
+
+  let d = new Date();
+  d.setTime(d.getTime() + 10000 * 900 * 20000);
 
   const hide = (el) => {
-    el.style.display = 'none';
-  }
+    el.style.display = "none";
+  };
 
   const show = (el) => {
-    el.style.display = 'block';
-  }
+    el.style.display = "block";
+  };
 
   useEffect(() => {
     axios.get("/api/items").then((response) => {
       setItems(response.data);
       console.log(response.data);
     });
-  }, []);
+    setHighScore(score);
+  }, [score]);
 
   const submitHandler = () => {
     if (index <= items.length - 1) {
@@ -46,20 +55,22 @@ const LandingPage = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    if (answer === "" || answer != items[index].name.toLowerCase()) {
-      toast.error("Incorrect", {
-        className: "toast-failed",
-        bodyClassName: "toast-failed",
-        theme: "colored",
-      });
+    if (answer === "" || answer !== items[index].name.toLowerCase()) {
+      hide(form);
+      setAnswer("");
+      setScore(0);
+      setTimeout(() => {
+        submitHandler();
+      }, 1000);
     }
     if (answer === items[index].name.toLowerCase()) {
-      hide(form)
-      setScore(score + 1)
-      show(itemName)
-      toast.success("Correct!", {
-        icon: "+1"
-      });
+      hide(form);
+      setScore(score + 1);
+      setWobble(1)
+      show(itemName);
+    }
+    if (score > cookies.high_score) {
+      setCookie("high_score", highScore + 1, { path: "/", expires: d });
     }
   };
 
@@ -68,21 +79,42 @@ const LandingPage = () => {
       {items && items.length > 0 ? (
         <div id="container" key={items[index]._id}>
           <ToastContainer />
-          <h2 id="title">CS:GO Skindle</h2>
-          <p id="score">{score}</p>
+          <img id="logo" src={Logo} />
+          <div className="score_box">
+            <div className="score_detail">
+              <p>Score:</p>
+              <p id="score">{score}</p>
+              <p
+                id="float"
+                onAnimationEnd={() => setWobble(0)}
+                wobble={wobble}
+              >
+                +1
+              </p>
+            </div>
+            <div className="score_detail">
+              <p>High Score:</p>
+              <p id="score">{cookies.high_score}</p>
+            </div>
+          </div>
           <div className="slot">
             <div className="boxes">
               <div className="box">
                 <img id="image" src={items[index].img_url} alt="profile img" />
+                <img id="image" src={items[index].img_url} alt="profile img" />
+                <img id="image" src={items[index].img_url} alt="profile img" />
               </div>
-              <p id="item_name" style={{display: 'none'}}>✅ {items[index].name}</p>
             </div>
+            <p id="item_name" style={{ display: "none" }}>
+              ✅ {items[index].name}
+            </p>
           </div>
-          <div id="form" style={{display: 'block'}}>
+          <div id="form" style={{ display: "block" }}>
             <input
               id="input"
               placeholder="Guess the Item Skin"
               onChange={onChange}
+              autoComplete="false"
             ></input>
             <button id="submit" onClick={(e) => onSubmit(e)}>
               Submit
