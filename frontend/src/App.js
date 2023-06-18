ed3e21dccfec03f95a49a164641284c446cec552
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2023-06-18 16:48:26 -0400

    loader changes and react analytics setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,14 @@ import { React, Suspense, lazy } from "react";
 import "./style.scss";
 import { ReactComponent as RollingLoader } from "./static/rolling.svg";
 import "react-toastify/dist/ReactToastify.css";
+import ReactGA from "react-ga4";
 
 const LandingPage = lazy(() => import("./pages/LandingPage"));
 const Leaderboard = lazy(() => import("./pages/Leaderboard"));
 
 const App = () => {
+    ReactGA.initialize('G-22B25JWC59');
+    ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <div className="app">
       <Routes>
